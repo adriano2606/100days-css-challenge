20590eedfef238f8c8406bde8bca2254d5948896
Adriano de Camargo <adrianodecamargo123@gmail.com>
2024-02-01 18:44:36 -0300

    Refatorando consumo do fetch e adicionando description

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,27 @@
 
-let numProjec = 4;
+let totalProjects = 4;
 let appSection = document.getElementById('app')
 
-for(let i = 1; i <= numProjec; i++){
-
-    async function callTitle() {
-        return await fetch(`https://adriano2606.github.io/100days-css-challenge/day0${i}`) 
+async function createCards() {
+    for (let i = 1; i <= totalProjects; i++) {
+        let dom = await fetch(`https://adriano2606.github.io/100days-css-challenge/day0${i}`)
         .then(response => {return response.text()})
         .then(html => {
-            let parser = new DOMParser();
-            let doc = parser.parseFromString(html, "text/html");
-            const titulo = doc.title;
-            return titulo
-        })
+            let newDOM = new DOMParser().parseFromString(html, "text/html")
+            return newDOM
+            })
+        
+        let projectDay = dom.title.slice(9)
+        let projectDescription = dom.title.slice(0,6)
+            
+        appSection.innerHTML += `<div class="card"><a href="https://adriano2606.github.io/100days-css-challenge/day0${i}" target="_blank">
+        <div class="card-txt">
+            <span>${projectDay}</span>
+            <span>${projectDescription}</span>
+        </div>
+        <img src="./assets/img/0${i}.PNG" alt="Imagem Projeto ${i}" class="img">
+    </a></div>`
     }
-    
-    function createCard() {
-        return callTitle().then(a => { 
-            return `<div class="card"><a href="https://adriano2606.github.io/100days-css-challenge/day0${i}" target="_blank">
-            <div class="card-txt">
-                <span>${a}</span>
-                <span>Animated sunrise/sunset</span>
-            </div>
-            <img src="./assets/img/0${i}.PNG" alt="Imagem Projeto ${i}" class="img">
-        </a></div>`
-        })
-    }
-    
-    createCard().then(a => {appSection.innerHTML += a})
+}       
 
-
-}
+createCards()
